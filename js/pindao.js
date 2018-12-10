aba0b676ebb4d4919f4e1b0138caddbdf4205005
Your Name <email@example.com>
2018-12-10 10:51:52 +0800

    enter

---
+++ js/pindao.js
@@ -0,0 +1,33 @@
+var m = 0;
+var _show = document.getElementById("smallshow");
+function move(ele, dir, end) {
+    if (dir === "left") {
+        ele.style.left = ele.offsetLeft - 390 + "px";
+        if (ele.offsetLeft <= end) {
+            ele.style.left = end + "px";
+        }
+    } else {
+        ele.style.left = ele.offsetLeft + 390 + "px";
+        if (ele.offsetLeft >= end) {
+            ele.style.left = end + "px";
+        }
+    }
+}
+$(".left-pointer").click(function () {
+    if (m > 1) {
+        _show.style.left = 0;
+        m = 0;
+    }
+    m++;
+    console.log(m);
+    move(_show,"left", -390 * m);
+})
+$(".right-pointer").click(function () {
+    if (m <= 0) {
+        _show.style.left = "-780px";
+        m = 1;
+    }
+    m--;
+    console.log(m);
+    move( _show,"right", -390 * m);
+})
