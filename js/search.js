959cc6901d24cb4350e89476db5c530dac5e995c
Your Name <email@example.com>
2018-12-13 14:20:15 +0800

    搜索框第一次完善

--- js/search.js
+++ js/search.js
@@ -1,23 +1,29 @@
 var search = document.getElementById('search');
 var searchMessage = document.getElementById('searchmessage');
+var _button = document.getElementById('button');
 var _B = document.getElementById('_B');
-search.onkeyup = function() {
+
+search.onkeydown = function () {
 	var ele = document.createElement("script");
 	searchMessage.style.display = "block";
 	ele.src = "https://ds.suning.cn/ds/his/new/6059949960-" + search.value +
 		"-0-1_0-fn.jsonp?callback=fn&_=1542874684235";
 	document.getElementsByTagName("head")[0].appendChild(ele);
 	document.getElementsByTagName("head")[0].removeChild(ele);
-	if (search.value ==="") {
+	if (search.value === "") {
 		searchMessage.style.display = "block";
+	}
+}
+search.onkeyup = function () {
+	_button.onclick = function () {
+		window.location.href = `https://search.suning.com/${search.value}/`;
 	}
 }
 
 function fn(data) {
 	var res = data.words;
 	var str = "";
-	res.forEach(function(item, index) {
-		console.log(item.keyword)
+	res.forEach(function (item, index) {
 		if (item.keyword !== undefined) {
 			str +=
 				`<li><a href="https://search.suning.com/${item.keyword}/">${item.keyword}</a></li>`
@@ -26,13 +32,12 @@ function fn(data) {
 	_B.innerHTML = str;
 }
 
-search.onfocus = function() {
+search.onfocus = function () {
 	searchMessage.style.display = "block";
-	console.log(1)
 }
-search.onblur = function() {
-	setTimeout(function(){
+search.onblur = function () {
+	setTimeout(function () {
 		searchMessage.style.display = "none";
-	},500)
-	
+	}, 500)
+
 }
